aff7bcd4530c9be507ae8c85242b4d77e7babd26
Ashutosh Tripathy <ashutosh@edmingle.com>
2024-04-10 11:34:58 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,78 @@
+const express= require("express");
+const cron =require("node-cron");
+const  { processPage,processPageQuiz } = require("./script.js");
+const env = require("dotenv").config().parsed;
+const app = express();
+
+// Variable to keep track of page number
+let pageNumber = 1;
+// let pageNumber2 = 1;
+
+function scheduleCronJob() {
+    // Define the cron job
+    const job = cron.schedule('*/30 * * * *', async () => {
+        // Get current hour
+        const currentHour = new Date().getHours();
+
+        // Only execute between 12 am and 6 am
+        if (currentHour >= 0 && currentHour <= 6 && pageNumber<=5000) {
+            // Call the function with the current page number
+           await processPage(pageNumber);
+            
+            // Increment page number for the next call
+            pageNumber++;
+        }
+    }, {
+        scheduled: true,
+        timezone: "Asia/Calcutta" // Replace "Your_Time_Zone" with your actual time zone
+    });
+
+    // Start the cron job
+    job.start();
+}
+
+
+//cron job for quiz
+
+// function scheduleAnotherCronJob() {
+//     // Define the second cron job
+//     const job = cron.schedule('*/15 * * * * *', async () => {
+//         // Get current hour
+//         const currentHour = new Date().getHours();
+//         console.log('Second cron job for quiz is running every 30 seconds...');
+
+//         // Only execute between 12 am and 6 am
+//         if (currentHour >= 18 && currentHour <= 23) {
+//             try {
+//                 // Call the function with the current page number
+//                 await processPageQuiz(pageNumber2);
+                
+//                 // Increment page number for the next call
+//                 pageNumber2++;
+//             } catch (error) {
+//                 console.error('Error processing page for second cron job:', error);
+//             }
+//         }
+//     }, {
+//         scheduled: true,
+//         timezone: "Asia/Calcutta" // Replace "Your_Time_Zone" with your actual time zone
+//     });
+
+//     // Start the second cron job
+//     job.start();
+// }
+
+// Call the function to schedule the cron job
+scheduleCronJob();
+
+// scheduleAnotherCronJob();
+
+app.get('/', (req, res) => {
+    res.send('Hello world');
+});
+
+// const PORT = 3000;
+
+app.listen(env.PORT, () => {
+    console.log(`Server is listening on port ${env.PORT}`);
+});
---
+++ script.js
@@ -0,0 +1,109 @@
+const FormData = require("form-data");
+// const axios = require("axios");
+// import fetch from "node-fetch";
+const fetch  = require("node-fetch");
+const env = require("dotenv").config().parsed;
+
+
+
+async function makeApiRequest(pageNumber) {
+    const apiUrl = 'http://localhost/nuSource/api/v1/migrate/studentbulktestupdate';
+    const apiKey = 'b291d4b81f41cf19f759ee5c319ef821';
+    const orgId = '2';
+
+    const headers = {                   
+        'authority': 'abhishek-api.edmingle.com',
+        'accept': 'application/json, text/javascript, */*; q=0.01',
+        'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+        'apikey': env.APIKEY,
+        'orgid': env.ORGID,
+        'origin': 'https://abhishek.edmingle.com',
+        'referer': 'https://abhishek.edmingle.com/',
+        'sec-ch-ua': '"Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Linux"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
+    };
+
+
+    const formData = new FormData();
+    var data = {};
+    data['page'] =  pageNumber;
+    data['per_page'] =  1;
+    data['run_migration'] =  0;
+    var JSONString = JSON.stringify(data);
+    formData.append('JSONString',JSONString);
+
+    return await fetch(env.APIURL, {
+        method: 'POST',
+        headers: headers,
+        body: formData,
+    })
+    .then(response => response.json())
+    .catch(error => {
+            console.error('Error for page', pageNumber, ':', error);
+            return { error: true, message: `Error occurred for page ${pageNumber}. Check console for details.` };
+        });
+}
+
+async function makeApiRequestForQuiz(pageNumber) {
+    const apiUrl = 'http://localhost/nuSource/api/v1/migrate/studentbulktestquizupdate';
+    const apiKey = 'b291d4b81f41cf19f759ee5c319ef821';
+    const orgId = '2';
+
+    const headers = {                   
+        'APIKEY': apiKey,
+        'Accept': 'application/json, text/javascript, */*; q=0.01',
+        'Accept-Language': 'en-GB,en;q=0.9',
+        'Connection': 'keep-alive',
+        'Cookie': 'G_ENABLED_IDPS=google; _clck=jjneul%7C2%7Cfhq%7C0%7C1450; _fw_crm_v=1be00437-dde8-480e-eebb-ce32da9659a0; _clsk=88jgca%7C1703137621624%7C4%7C1%7Ca.clarity.ms%2Fcollect',
+        'ORGID': env.ORGID,
+        'Origin': 'http://localhost',
+        'Referer': 'http://localhost/myaccount/',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-origin',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36',
+        'X-Requested-With': 'XMLHttpRequest',
+        'sec-ch-ua': '"Chromium";v="118", "Google Chrome";v="118", "Not=A?Brand";v="99"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Linux"',
+    };
+
+
+    const formData = new FormData();
+    var data = {};
+    data['page'] =  pageNumber;
+    data['per_page'] =  1;
+    data['run_migration'] =  1;
+    var JSONString = JSON.stringify(data);
+    formData.append('JSONString',JSONString);
+
+    return await fetch(apiUrl, {
+        method: 'POST',
+        headers: headers,
+        body: formData,
+    })
+    .then(response => response.json())
+    .catch(error => {
+            console.error('Error for page', pageNumber, ':', error);
+            return { error: true, message: `Error occurred for page ${pageNumber}. Check console for details.` };
+        });
+}
+
+exports.processPage= async (pageNumber)=> {
+   
+   response= await makeApiRequest(pageNumber);
+    return 1;
+   
+}
+
+exports.processPageQuiz= async (pageNumber)=> {
+   
+    response= await makeApiRequestForQuiz(pageNumber);
+     return 1;
+    
+ }

a6d7c3b1544f9d81c86268fbd89d08020520f144
Ashutosh Tripathy <ashutosh@edmingle.com>
2024-04-18 23:25:41 +0530

    changes in page number

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const env = require("dotenv").config().parsed;
 const app = express();
 
 // Variable to keep track of page number
-let pageNumber = 304646;
+let pageNumber = 304845;
 let response = 0;
 let count = 0;
 let sleeptimecount = 0;        
@@ -77,12 +77,12 @@ async function scheduleCronJob() {
                     // process.exit(0);
                 }
                 // Increment page number for the next call
-                pageNumber= pageNumber+200;
+                pageNumber= pageNumber+150;
                  count += 1 ; 
                  sleeptimecount +=1;
                  if(sleeptimecount >=10){
                     const htmlmigrationCount = `
-                        <h1> ${pageNumber-200} deta has been migrated</h1>
+                        <h1> ${pageNumber-150} deta has been migrated</h1>
                         <p>last_page_number ${pageNumber}</p>
                         `;
                     mailerRes = await mailerNode(htmlmigrationCount).catch(e => console.log(e));
